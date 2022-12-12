cb079dd1694cdd5f72f2d9305a6cdae0b2e9822c
yami2200 <TheYami22@gmail.com>
2022-12-12 15:05:58 +0100

    Add TailwindCSS and DaisyUI.

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div class="drawer drawer-end">
+      <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content">
+        <!-- Page content here -->
+        <label class="btn btn-circle" for="my-drawer-4">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="5 5 36 36" stroke="currentColor"><path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z"/></svg>
+        </label>
+      </div>
+      <div class="drawer-side">
+        <label for="my-drawer-4" class="drawer-overlay"></label>
+        <ul class="menu p-4 w-80 bg-base-100 text-base-content">
+          <!-- Sidebar content here -->
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
