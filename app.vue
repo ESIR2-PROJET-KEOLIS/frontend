b2276c474e36641ac9a4b136180b76ef83750b9e
yami2200 <theyami22@gmail.com>
2022-12-13 11:07:46 +0100

    Add Drawer Component.

--- app.vue
+++ app.vue
@@ -1,25 +1,23 @@
 <template>
   <div>
-    <div class="drawer drawer-end">
-      <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content">
-        <!-- Page content here -->
+    <drawer drawerFor="my-drawer">
+
+      <template #page-content>
         <map-box></map-box>
         <map-btn-grp drawerFor="my-drawer" id="buttons"></map-btn-grp>
-      </div>
-      <div class="drawer-side">
-        <label for="my-drawer" class="drawer-overlay"></label>
-        <div class="menu p-4 w-90 bg-base-100 text-base-content">
-          <h1 class="text-center text-lg mb-1">Visualisation en Temps réel</h1>
-          <toggle-btn text="Temps Réel" v-model:checked="realTime"></toggle-btn>
-          <div class="divider"></div>
-          <h1 class="text-center text-lg mb-4"> Simulation</h1>
-          <day-selector :disabled="realTime" :checkIndex="currentDaySimulated" @change="changeDay"></day-selector>
-          <slider :disabled="realTime" :value="simulatedHourSlider" :min="0" :max="1439" @change="changeTime"></slider>
-          <p class="text-center text-lg mb-4"> {{simulatedHour}} </p>
-        </div>
-      </div>
-    </div>
+      </template>
+
+      <template #drawer-content>
+        <h1 class="text-center text-lg mb-1">Visualisation en Temps réel</h1>
+        <toggle-btn text="Temps Réel" v-model:checked="realTime"></toggle-btn>
+        <div class="divider"></div>
+        <h1 class="text-center text-lg mb-4"> Simulation</h1>
+        <day-selector :disabled="realTime" :checkIndex="currentDaySimulated" @change="changeDay"></day-selector>
+        <slider :disabled="realTime" :value="simulatedHourSlider" :min="0" :max="1439" @change="changeTime"></slider>
+        <p class="text-center text-lg mb-4"> {{simulatedHour}} </p>
+      </template>
+
+    </drawer>
   </div>
 </template>
 
@@ -41,6 +39,7 @@ import { SimulationInfo } from '@/classes/SimulationInfo';
 import {MapBoxComponent, MapButtonGroup, DaySelector} from "#components";
 import ToggleButton from "~/components/generic/ToggleButton.vue";
 import Slider from "~/components/generic/Slider.vue";
+import Drawer from "~/components/generic/Drawer.vue";
 
 export default defineComponent({
   data: () => ({
@@ -54,6 +53,7 @@ export default defineComponent({
     "toggle-btn" : ToggleButton,
     "day-selector" : DaySelector,
     "slider" : Slider,
+    "drawer" : Drawer,
   },
   computed:{
     simulatedHour() : string{
---
+++ components/generic/Drawer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="drawer drawer-end">
+    <input :id="drawerFor" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <slot name="page-content"></slot>
+    </div>
+    <div class="drawer-side">
+      <label :for="drawerFor" class="drawer-overlay"></label>
+      <div class="menu p-4 w-90 bg-base-100 text-base-content">
+        <slot name="drawer-content"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Drawer",
+  props: {
+    drawerFor: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
