0e4f7e1191dde76d2840ce774b9ebd9860981e65
yami2200 <theyami22@gmail.com>
2022-12-13 08:34:55 +0100

    Add map buttons group component.

--- app.vue
+++ app.vue
@@ -1,21 +1,14 @@
 <template>
   <div>
     <div class="drawer drawer-end">
-      <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+      <input id="my-drawer" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content">
         <!-- Page content here -->
         <map-box></map-box>
-        <div class="btn-group" id="button">
-          <label class="btn">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="5 5 36 36" stroke="currentColor"><path d="M12.45 42q-.65 0-1.15-.375-.5-.375-.5-.975v-4.2q-1.45-.8-2.125-2.3Q8 32.65 8 30.95V11.1q0-3.7 3.825-5.4Q15.65 4 24.05 4q8.3 0 12.125 1.7Q40 7.4 40 11.1v19.85q0 1.7-.675 3.2-.675 1.5-2.125 2.3v4.2q0 .6-.5.975T35.55 42h-.95q-.7 0-1.2-.375t-.5-.975V37.9H15.1v2.75q0 .6-.5.975T13.4 42Zm11.6-32.2H37 11h13.05Zm8.85 14.65H11h26-4.1Zm-21.9-3h26V12.8H11Zm5.3 10.95q1.15 0 1.95-.8t.8-1.95q0-1.15-.8-1.95t-1.95-.8q-1.15 0-1.95.8t-.8 1.95q0 1.15.8 1.95t1.95.8Zm15.4 0q1.15 0 1.95-.8t.8-1.95q0-1.15-.8-1.95t-1.95-.8q-1.15 0-1.95.8t-.8 1.95q0 1.15.8 1.95t1.95.8ZM11 9.8h26q-1.2-1.3-4.6-2.05Q29 7 24.05 7q-5.9 0-9.05.675-3.15.675-4 2.125Zm4.1 25.1h17.8q1.75 0 2.925-1.35Q37 32.2 37 30.45v-6H11v6q0 1.75 1.175 3.1T15.1 34.9Z"/></svg>
-          </label>
-          <label class="btn" for="my-drawer-4">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="5 5 36 36" stroke="currentColor"><path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z"/></svg>
-          </label>
-        </div>
+        <map-btn-grp drawerFor="my-drawer" id="buttons"></map-btn-grp>
       </div>
       <div class="drawer-side">
-        <label for="my-drawer-4" class="drawer-overlay"></label>
+        <label for="my-drawer" class="drawer-overlay"></label>
         <div class="menu p-4 w-90 bg-base-100 text-base-content">
           <h1 class="text-center text-lg mb-1">Visualisation en Temps réel</h1>
           <label class="label cursor-pointer">
@@ -44,7 +37,7 @@
 </template>
 
 <style>
-#button {
+#buttons {
   position: fixed;
   top: 1rem;
   right: 1rem;
@@ -54,17 +47,12 @@
 
 <script>
 import MapBoxComponent from "~/components/MapBoxComponent.vue";
-/* useAsyncData()and $fetch() are auto-imported */
-//const { data, refresh, pending } = await useAsyncData('/api/hello', () => $fetch('/api/hello'))
+import MapButtonGroup from "~/components/MapButtonGroup.vue";
 
 export default {
-  data() {
-    return {
-      count: 0
-    }
-  },
   components: {
     "map-box" : MapBoxComponent,
+    "map-btn-grp" : MapButtonGroup
   },
 }
 
