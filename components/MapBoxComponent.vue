6d022061a10afb602f6f4472b1a815ca538bfa9e
yami2200 <TheYami22@gmail.com>
2022-12-30 18:46:50 +0100

    Add first websocket client prototype.

--- components/MapBoxComponent.vue
+++ components/MapBoxComponent.vue
@@ -7,18 +7,22 @@
 <script>
 import mapboxgl from "mapbox-gl";
 import { BusLayer} from "~/classes/BusLayer";
+import emptyFeatureCollection from "~/classes/emptyFeatureCollection.json";
 
 export default {
   name: "MapBoxComponent",
   data: () => ({
     mapRef : null,
+    busses : JSON.parse(JSON.stringify(emptyFeatureCollection)),
+    bus_stops : JSON.parse(JSON.stringify(emptyFeatureCollection)),
+    subway_stops : JSON.parse(JSON.stringify(emptyFeatureCollection)),
+    initialized : false,
   }),
   watch: {
     busLines: {
       handler(val, oldVal) {
         for(let i = 0; i<this.busLines.length; i++){
           this.mapRef.setLayoutProperty(this.busLines[i].layerID, 'visibility', this.busLines[i].visible ? 'visible' : 'none');
-          console.log(this.busLines[i].layerID + " : " + this.busLines[i].visible);
         }
       },
       deep: true
@@ -30,10 +34,82 @@ export default {
       default: []
     }
   },
-  mounted() {
-    const runtimeConfig = useRuntimeConfig();
+  methods:{
+    loadMapFirstTime(data){
+      const runtimeConfig = useRuntimeConfig();
+      this.initialized = true;
 
-    const data = {
+      mapboxgl.accessToken = runtimeConfig.public.mapboxAccessToken;
+      this.mapRef = new mapboxgl.Map({
+        container: 'mapDiv',
+        style: 'mapbox://styles/jeanbombeur/clbm382qs000l14mu0ayx6zxi',
+        center: [-1.656946, 48.114572],
+        zoom:13,
+        minzoom: 16,
+        maxPitch: 0,
+        minPitch: 0,
+        maxBounds: new mapboxgl.LngLatBounds(
+            new mapboxgl.LngLat(-1.776660, 48.044477), //48.044477, -1.776660
+            new mapboxgl.LngLat(-1.563988, 48.188023) //48.188023, -1.563988
+        ),
+      });
+
+      this.busses = data;
+
+      this.mapRef.on('load', () => {
+        this.mapRef.addSource('busses', {
+          'type': 'geojson',
+          'data': this.busses
+        });
+
+        this.mapRef.addSource('busStop', {
+          'type': 'geojson',
+          'data': this.bus_stops
+        });
+
+        this.mapRef.addSource('subwayStation', {
+          'type': 'geojson',
+          'data': this.subway_stops
+        });
+
+        for(const feature of data.features) {
+          const symbol = feature.properties.icon;
+          const layerID = "bus-line-" + feature.properties.line;
+
+          if (!this.mapRef.getLayer(layerID)) {
+            this.mapRef.addLayer({
+              'id': layerID,
+              'type': 'symbol',
+              'source': 'busses',
+              'layout': {
+                'icon-image': `${symbol}`,
+                'icon-allow-overlap': true
+              },
+              'filter': ['==', 'line', layerID.split("-")[2]]
+            });
+            this.busLines.push(new BusLayer(layerID, feature.properties.line));
+          }
+        }
+      });
+    },
+    updateMap(data){
+      this.mapRef.getSource('busses').setData(data);
+    },
+  },
+  mounted() {
+    let webSocket = new WebSocket("ws://localhost:4000");
+
+    webSocket.onopen = (event) => {
+      webSocket.send("bus");
+      console.log("WebSocket Connection established");
+    };
+
+    webSocket.onmessage = (event) => {
+      if(!this.initialized) this.loadMapFirstTime(JSON.parse(event.data));
+      else this.updateMap(JSON.parse(event.data));
+    }
+
+    /*const data = {
       'type': 'FeatureCollection',
       'features': [
         {
@@ -93,42 +169,12 @@ export default {
           }
         },
       ]
-    };
+    };*/
 
 
+      /**/
 
-    mapboxgl.accessToken = runtimeConfig.public.mapboxAccessToken;
-    this.mapRef = new mapboxgl.Map({
-      container: 'mapDiv',
-      style: 'mapbox://styles/jeanbombeur/clbm382qs000l14mu0ayx6zxi',
-      center: [-1.656946, 48.114572],
-      zoom:13,
-      minzoom: 16,
-      maxPitch: 0,
-      minPitch: 0,
-      maxBounds: new mapboxgl.LngLatBounds(
-          new mapboxgl.LngLat(-1.776660, 48.044477), //48.044477, -1.776660
-          new mapboxgl.LngLat(-1.563988, 48.188023) //48.188023, -1.563988
-      ),
-    });
-
-    this.mapRef.on('load', () => {
-      this.mapRef.addSource('testData', {
-        'type': 'geojson',
-        'data': data
-      });
-
-      this.mapRef.addSource('busStop', {
-        'type': 'geojson',
-        'data': bus_stop_data
-      });
-
-      this.mapRef.addSource('subwayStation', {
-        'type': 'geojson',
-        'data': subway_station_data
-      });
-
-      if (!this.mapRef.getLayer("bus-stop")) this.mapRef.addLayer({
+      /*if (!this.mapRef.getLayer("bus-stop")) this.mapRef.addLayer({
         'id': "bus-stop",
         'type': 'symbol',
         'source': 'busStop',
@@ -148,27 +194,7 @@ export default {
           'icon-allow-overlap': true
         },
         'filter': ['==', 'icon', "subway"]
-      });
-
-      for(const feature of data.features) {
-        const symbol = feature.properties.icon;
-        const layerID = "bus-line-" + feature.properties.line;
-
-        if (!this.mapRef.getLayer(layerID)) {
-          this.mapRef.addLayer({
-            'id': layerID,
-            'type': 'symbol',
-            'source': 'testData',
-            'layout': {
-              'icon-image': `${symbol}`,
-              'icon-allow-overlap': true
-            },
-            'filter': ['==', 'line', layerID.split("-")[2]]
-          });
-          this.busLines.push(new BusLayer(layerID, feature.properties.line));
-        }
-      }
-    });
+      });*/
   }
 }
 </script>
