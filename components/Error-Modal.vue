8c34d161859657a1e88521c9b87f6ad22c33de50
yami2200 <TheYami22@gmail.com>
2023-04-23 18:48:37 +0200

    handle multiple errors displayed at the same time with a queue system.

--- components/Error-Modal.vue
+++ components/Error-Modal.vue
@@ -6,7 +6,7 @@
             <p class="py-4"> {{ errormsg }}</p>
             <div class="modal-action">
                 <label @click="refresh" class="btn">Refresh Page</label>
-                <label for="my-modal-error" class="btn">Close</label>
+                <label for="my-modal-error" class="btn" @click="closeModal">Close</label>
             </div>
         </div>
     </div>
@@ -18,12 +18,29 @@ export default {
     data() {
         return {
             errormsg : "",
+            listError: []
         }
     },
     methods: {
         showErrorMessage(message){
-            this.errormsg = message;
+            if(this.listError.length > 0){
+                this.listError.push(message);
+                return;
+            }
+            this.listError.push(message);
+            this.setMessageAndOpenModal();
+        },
+        setMessageAndOpenModal(){
+            if(this.listError.length === 0) return;
+            this.errormsg = this.listError[0];
             document.getElementById('my-modal-error').checked = true;
+        },
+        closeModal(){
+          let index = this.listError.indexOf(this.errormsg);
+          this.listError.splice(index, 1);
+          if(this.listError.length > 0){
+              setTimeout(this.setMessageAndOpenModal, 250);
+          }
         },
         refresh(){
             window.location.reload();
